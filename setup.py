a1578dbce22b312d0b7ab279d3a77342b7ce336c
Paul Marshall <paul.marshall@colorado.edu>
2013-04-11 14:01:02 -0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "phorque",
+    version = "0.1",
+    scripts = ["bin/phorque.py"],
+    packages = find_packages(),
+    author = "Paul Marshall",
+    author_email = "paul.marshall@colorado.edu",
+    license=open("LICENSE.txt").read(),
+)
